6e06821a5cc99b4c0dcc15c161b26dd9f82b735f
bbyer04 <143546151+bbyer04@users.noreply.github.com>
2025-05-09 11:52:13 -0400

    new

--- requests/requests.py
+++ requests/requests.py
@@ -1,3 +1,6 @@
 import requests
+import json
+url = ('https://newsapi.org/v2/top-headlines?country=us&category=science&apiKey=2f40e89b12054edbba8eab400c7b5c82')
 
-# new file
+response = requests.get(url)
+print(response.json())
